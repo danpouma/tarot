69335fa50332c7694bffe239a111b10fc02330fc
danpouma <dpoumakis@yahoo.com>
2016-03-25 15:49:59 -0400

    Initial commit

---
+++ src/tarot/Tarot.java
@@ -0,0 +1,32 @@
+package tarot;
+
+/*
+TODO:
+Objective: dealing with linked lists
+
+Now we are going to read fortunes: Tarot cards. The suits this time are swords, 
+wands, coins and cups. The face values stay the same but there are 14 cards to 
+a suit (King, Queen, Knight & Jack).
+
+Shuffle the deck and place nine cards in a circle. Randomly pick a number 1-9. 
+Count to that number going around the circle. When you get to that number card, 
+print what the card is and replace it in the circle with the next card on the 
+deck. At any given time, there are nine cards in the circle and you have a 
+“home card” (The one that tells your fate). A move consists of picking a random 
+number, removing a card and replacing it with the next card on the deck. Print 
+the card that was removed and the card that replaced it.
+*/
+
+
+/**
+ *
+ * @author dpoumakis
+ */
+public class Tarot
+{
+    public static void main(String[] args)
+    {
+        // Dee
+    }
+    
+}
